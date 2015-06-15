2eca7f5a447beb6b1f3f7d449f0a18a22a78ee21
孟君 <mengjun1990@gmail.com>
2015-06-15 14:38:44 +0800

    up

--- Modules/User/Tpl/Reg/index.php
+++ Modules/User/Tpl/Reg/index.php
@@ -46,14 +46,14 @@
 </div>
 <div class="square-layout">
 	<ul>
-		<li onclick="goUrl('/wap.php?c=cp_info');"><span><img src="/tpl/wap/images/1121789.png" />我的资料</span></li>
-		<li onclick="goUrl('/wap.php?c=cp_match');"><span><img src="/tpl/wap/images/1121884.png" />缘分速配</span></li>
-		<li onclick="goUrl('/wap.php?c=cp_message');"><span><img src="/tpl/wap/images/1121746.png" />收件箱</span></li>
-		<li onclick="goUrl('/wap.php?c=cp_outmsg');"><span><img src="/tpl/wap/images/1121867.png" />发件箱</span></li>
-		<li onclick="goUrl('/wap.php?c=cp_visitme');"><span><img src="/tpl/wap/images/1121945.png" />谁看过我</span></li>
-		<li onclick="goUrl('/wap.php?c=cp_visit');"><span><img src="/tpl/wap/images/1121946.png" />我看过谁</span></li>
-		<li onclick="goUrl('/wap.php?c=cp_fans');"><span><img src="/tpl/wap/images/1121949.png" />我的粉丝</span></li>
-		<li onclick="goUrl('/wap.php?c=cp_listen');"><span><img src="/tpl/wap/images/1121950.png" />我的关注</span></li>
+		<li onclick="goUrl('/wap.php?c=cp_info');"><span><img src="__PUBLIC__/images/1121789.png" />我的资料</span></li>
+		<li onclick="goUrl('/wap.php?c=cp_match');"><span><img src="__PUBLIC__/images/1121884.png" />缘分速配</span></li>
+		<li onclick="goUrl('/wap.php?c=cp_message');"><span><img src="__PUBLIC__/images/1121746.png" />收件箱</span></li>
+		<li onclick="goUrl('/wap.php?c=cp_outmsg');"><span><img src="__PUBLIC__/images/1121867.png" />发件箱</span></li>
+		<li onclick="goUrl('/wap.php?c=cp_visitme');"><span><img src="__PUBLIC__/images/1121945.png" />谁看过我</span></li>
+		<li onclick="goUrl('/wap.php?c=cp_visit');"><span><img src="__PUBLIC__/images/1121946.png" />我看过谁</span></li>
+		<li onclick="goUrl('/wap.php?c=cp_fans');"><span><img src="__PUBLIC__/images/1121949.png" />我的粉丝</span></li>
+		<li onclick="goUrl('/wap.php?c=cp_listen');"><span><img src="__PUBLIC__/images/1121950.png" />我的关注</span></li>
 	</ul>
 	<div class="clear"></div>
 </div>
