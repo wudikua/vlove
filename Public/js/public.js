c86a9ee26ade8ee75b13c2da7187ddf854f9149a
孟君 <mengjun1990@gmail.com>
2015-06-21 22:03:57 +0800

    zepto 增加fx_methods fx selector模块

--- Public/js/public.js
+++ Public/js/public.js
@@ -294,6 +294,206 @@ function hometownPopup(title, item) {
 		}
 	});
 }
+;(function($){
+    var zepto = $.zepto, oldQsa = zepto.qsa, oldMatches = zepto.matches
+
+    function visible(elem){
+        elem = $(elem)
+        return !!(elem.width() || elem.height()) && elem.css("display") !== "none"
+    }
+
+    // Implements a subset from:
+    // http://api.jquery.com/category/selectors/jquery-selector-extensions/
+    //
+    // Each filter function receives the current index, all nodes in the
+    // considered set, and a value if there were parentheses. The value
+    // of `this` is the node currently being considered. The function returns the
+    // resulting node(s), null, or undefined.
+    //
+    // Complex selectors are not supported:
+    //   li:has(label:contains("foo")) + li:has(label:contains("bar"))
+    //   ul.inner:first > li
+    var filters = $.expr[':'] = {
+        visible:  function(){ if (visible(this)) return this },
+        hidden:   function(){ if (!visible(this)) return this },
+        selected: function(){ if (this.selected) return this },
+        checked:  function(){ if (this.checked) return this },
+        parent:   function(){ return this.parentNode },
+        first:    function(idx){ if (idx === 0) return this },
+        last:     function(idx, nodes){ if (idx === nodes.length - 1) return this },
+        eq:       function(idx, _, value){ if (idx === value) return this },
+        contains: function(idx, _, text){ if ($(this).text().indexOf(text) > -1) return this },
+        has:      function(idx, _, sel){ if (zepto.qsa(this, sel).length) return this }
+    }
+
+    var filterRe = new RegExp('(.*):(\\w+)(?:\\(([^)]+)\\))?$\\s*'),
+        childRe  = /^\s*>/,
+        classTag = 'Zepto' + (+new Date())
+
+    function process(sel, fn) {
+        // quote the hash in `a[href^=#]` expression
+        sel = sel.replace(/=#\]/g, '="#"]')
+        var filter, arg, match = filterRe.exec(sel)
+        if (match && match[2] in filters) {
+            filter = filters[match[2]], arg = match[3]
+            sel = match[1]
+            if (arg) {
+                var num = Number(arg)
+                if (isNaN(num)) arg = arg.replace(/^["']|["']$/g, '')
+                else arg = num
+            }
+        }
+        return fn(sel, filter, arg)
+    }
+
+    zepto.qsa = function(node, selector) {
+        return process(selector, function(sel, filter, arg){
+            try {
+                var taggedParent
+                if (!sel && filter) sel = '*'
+                else if (childRe.test(sel))
+                // support "> *" child queries by tagging the parent node with a
+                // unique class and prepending that classname onto the selector
+                    taggedParent = $(node).addClass(classTag), sel = '.'+classTag+' '+sel
+
+                var nodes = oldQsa(node, sel)
+            } catch(e) {
+                console.error('error performing selector: %o', selector)
+                throw e
+            } finally {
+                if (taggedParent) taggedParent.removeClass(classTag)
+            }
+            return !filter ? nodes :
+                zepto.uniq($.map(nodes, function(n, i){ return filter.call(n, i, nodes, arg) }))
+        })
+    }
+
+    zepto.matches = function(node, selector){
+        return process(selector, function(sel, filter, arg){
+            return (!sel || oldMatches(node, sel)) &&
+                (!filter || filter.call(node, null, arg) === node)
+        })
+    }
+})(Zepto)
+;(function($, undefined){
+    var prefix = '', eventPrefix,
+        vendors = { Webkit: 'webkit', Moz: '', O: 'o' },
+        testEl = document.createElement('div'),
+        supportedTransforms = /^((translate|rotate|scale)(X|Y|Z|3d)?|matrix(3d)?|perspective|skew(X|Y)?)$/i,
+        transform,
+        transitionProperty, transitionDuration, transitionTiming, transitionDelay,
+        animationName, animationDuration, animationTiming, animationDelay,
+        cssReset = {}
+
+    function dasherize(str) { return str.replace(/([a-z])([A-Z])/, '$1-$2').toLowerCase() }
+    function normalizeEvent(name) { return eventPrefix ? eventPrefix + name : name.toLowerCase() }
+
+    $.each(vendors, function(vendor, event){
+        if (testEl.style[vendor + 'TransitionProperty'] !== undefined) {
+            prefix = '-' + vendor.toLowerCase() + '-'
+            eventPrefix = event
+            return false
+        }
+    })
+
+    transform = prefix + 'transform'
+    cssReset[transitionProperty = prefix + 'transition-property'] =
+        cssReset[transitionDuration = prefix + 'transition-duration'] =
+            cssReset[transitionDelay    = prefix + 'transition-delay'] =
+                cssReset[transitionTiming   = prefix + 'transition-timing-function'] =
+                    cssReset[animationName      = prefix + 'animation-name'] =
+                        cssReset[animationDuration  = prefix + 'animation-duration'] =
+                            cssReset[animationDelay     = prefix + 'animation-delay'] =
+                                cssReset[animationTiming    = prefix + 'animation-timing-function'] = ''
+
+    $.fx = {
+        off: (eventPrefix === undefined && testEl.style.transitionProperty === undefined),
+        speeds: { _default: 400, fast: 200, slow: 600 },
+        cssPrefix: prefix,
+        transitionEnd: normalizeEvent('TransitionEnd'),
+        animationEnd: normalizeEvent('AnimationEnd')
+    }
+
+    $.fn.animate = function(properties, duration, ease, callback, delay){
+        if ($.isFunction(duration))
+            callback = duration, ease = undefined, duration = undefined
+        if ($.isFunction(ease))
+            callback = ease, ease = undefined
+        if ($.isPlainObject(duration))
+            ease = duration.easing, callback = duration.complete, delay = duration.delay, duration = duration.duration
+        if (duration) duration = (typeof duration == 'number' ? duration :
+            ($.fx.speeds[duration] || $.fx.speeds._default)) / 1000
+        if (delay) delay = parseFloat(delay) / 1000
+        return this.anim(properties, duration, ease, callback, delay)
+    }
+
+    $.fn.anim = function(properties, duration, ease, callback, delay){
+        var key, cssValues = {}, cssProperties, transforms = '',
+            that = this, wrappedCallback, endEvent = $.fx.transitionEnd,
+            fired = false
+
+        if (duration === undefined) duration = $.fx.speeds._default / 1000
+        if (delay === undefined) delay = 0
+        if ($.fx.off) duration = 0
+
+        if (typeof properties == 'string') {
+            // keyframe animation
+            cssValues[animationName] = properties
+            cssValues[animationDuration] = duration + 's'
+            cssValues[animationDelay] = delay + 's'
+            cssValues[animationTiming] = (ease || 'linear')
+            endEvent = $.fx.animationEnd
+        } else {
+            cssProperties = []
+            // CSS transitions
+            for (key in properties)
+                if (supportedTransforms.test(key)) transforms += key + '(' + properties[key] + ') '
+                else cssValues[key] = properties[key], cssProperties.push(dasherize(key))
+
+            if (transforms) cssValues[transform] = transforms, cssProperties.push(transform)
+            if (duration > 0 && typeof properties === 'object') {
+                cssValues[transitionProperty] = cssProperties.join(', ')
+                cssValues[transitionDuration] = duration + 's'
+                cssValues[transitionDelay] = delay + 's'
+                cssValues[transitionTiming] = (ease || 'linear')
+            }
+        }
+
+        wrappedCallback = function(event){
+            if (typeof event !== 'undefined') {
+                if (event.target !== event.currentTarget) return // makes sure the event didn't bubble from "below"
+                $(event.target).unbind(endEvent, wrappedCallback)
+            } else
+                $(this).unbind(endEvent, wrappedCallback) // triggered by setTimeout
+
+            fired = true
+            $(this).css(cssReset)
+            callback && callback.call(this)
+        }
+        if (duration > 0){
+            this.bind(endEvent, wrappedCallback)
+            // transitionEnd is not always firing on older Android phones
+            // so make sure it gets fired
+            setTimeout(function(){
+                if (fired) return
+                wrappedCallback.call(that)
+            }, ((duration + delay) * 1000) + 25)
+        }
+
+        // trigger page reflow so new elements can animate
+        this.size() && this.get(0).clientLeft
+
+        this.css(cssValues)
+
+        if (duration <= 0) setTimeout(function() {
+            that.each(function(){ wrappedCallback.call(this) })
+        }, 0)
+
+        return this
+    }
+
+    testEl = null
+})(Zepto)
 
 ;(function($, undefined){
     var document = window.document, docElem = document.documentElement,
