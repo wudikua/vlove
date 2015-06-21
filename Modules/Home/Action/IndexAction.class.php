b7368bfb8426ea944206fb015a4603280cfa2f74
孟君 <mengjun1990@gmail.com>
2015-06-21 23:50:03 +0800

    增加首页banner

--- Modules/Home/Action/IndexAction.class.php
+++ Modules/Home/Action/IndexAction.class.php
@@ -11,7 +11,7 @@ class IndexAction extends BaseAction {
 			$query['gender'] = $_COOKIE['gender'] == '2' ? '1' : '2';
 		}
 		// 首页推荐一些最新用户
-		$rt = MongoFactory::table("user")->find()->sort(['login_time'=>1])->limit(10);
+		$rt = MongoFactory::table("user")->find($query)->sort(['login_time'=>1])->limit(10);
 		$users = MongoUtil::asList($rt);
 		$users = array_merge($users, $users);
 		$users = array_merge($users, $users);
