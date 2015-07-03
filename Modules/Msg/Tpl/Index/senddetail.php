cf752bf02ee73160342a490618704a5f60c5e80b
zhaozhiqiang <zhaozhiqiang@qqzuche.com>
2015-07-03 11:14:06 +0800

    zzq

--- Modules/Msg/Tpl/Index/senddetail.php
+++ Modules/Msg/Tpl/Index/senddetail.php
@@ -7,14 +7,14 @@
             <img src="<?php if(strlen($result['fromuser']['avatar'])){?>__PUBLIC__/upload/thumb/s_<?php echo $result['fromuser']['avatar'];?><?php }else{?>__PUBLIC__/images/gender_1.gif<?php } ?>" />
         </div>
         <div class="cp-data-info">
-            <h2><b><?php echo $result['username'];?></b>(<?php if($result['type'] == 2){echo '发';}else{echo '收';}?>件人)</h2>
+            <h2><b><?php echo $result['fromuser']['username'];?></b>(<?php if($result['type'] == 2){echo '发';}else{echo '收';}?>件人)</h2>
             <ul>
                 <li>
-                    <?php echo ProfileConst::$gender[$result['gender']];?>  <?php echo age($result['fromuser']['birthday']);?> 岁 <?php ProfileConst::$marrystatus[$result['fromuser']['marrystatus']];?>
+                    <?php echo ProfileConst::$gender[$result['fromuser']['gender']];?>  <?php echo age($result['fromuser']['birthday']);?> 岁 <?php echo ProfileConst::$marrystatus[$result['fromuser']['marrystatus']];?>
                 </li>
                 <li>
                     <?php echo $result['fromuser']['city'] . '  ' .$result['fromuser']['area'];?>
-                    <?php echo ProfileConst::$education[$result['education'] ? $result['education'] : 1];?>
+                    <?php echo ProfileConst::$education[$result['fromuser']['education'] ? $result['fromuser']['education'] : 1];?>
                 </li>
             </ul>
             <div class="clear"></div>
