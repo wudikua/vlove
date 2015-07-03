03615d8a4a1feede1dd77ec3672ee7e949f39352
孟君 <mengjun1990@gmail.com>
2015-07-03 21:51:05 +0800

    banner

--- gate.php
+++ gate.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_REQUEST['backurl'])) {
-	$backurl = base64_decode($_REQUEST['backurl']);
+	$backurl = urlencode(base64_decode($_REQUEST['backurl']));
 } else {
 	$backurl = "http%3A%2F%2Fwap.datougou.cn%2Findex.php%2Fuser%2Findex%2Findex.html";
 }
