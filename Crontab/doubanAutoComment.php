500215cd55cd0e380072ea34e8efebc339561c98
孟君 <mengjun1990@gmail.com>
2015-07-06 20:57:32 +0800

    bind

--- Crontab/doubanAutoComment.php
+++ Crontab/doubanAutoComment.php
@@ -23,18 +23,17 @@ function upOneTopic($topicId) {
 	$curl->setUserAgent('api-client/2.0 com.douban.group/3.3.9(339) Android/22 hammerhead LGE Nexus 5');
 	$curl->setHeader("Authorization", "Bearer 68e7437c4512891e3ab5ca74cf379b5a");
 	$curl->setOpt(CURLOPT_SSL_VERIFYPEER, false);
+	$text = [
+		'坚持不懈的顶上去', '关注一个买不了吃亏买不了上当',
+		'微信关注wxdanshenba', '做个小站不容易',
+		'我真的不是发广告','这样吧不会被封号吧','人数突破500~\(≧▽≦)/~啦啦啦',
+		'写写大家捧场，自己也是单身狗，激烈些资源',
+		'纯属为大家义务劳动'
+	];
 	$rt = $curl->post(
 		'https://api.douban.com/v2/group/topic/'.$topicId.'/add_comment?udid=07a7d18a9f38be8932ed1a41fad89391ded77aae',
 		[
-			'content'=>array_rand(
-				[
-					'坚持不懈的顶上去', '关注一个买不了吃亏买不了上当',
-					'微信关注wxdanshenba', '做个小站不容易',
-					'我真的不是发广告','这样吧不会被封号吧','人数突破500~\(≧▽≦)/~啦啦啦',
-					'写写大家捧场，自己也是单身狗，激烈些资源',
-					'纯属为大家义务劳动'
-				]
-			)
+			'content'=>$text[array_rand($text)]
 		]
 	);
 	echo json_encode($rt).PHP_EOL;
