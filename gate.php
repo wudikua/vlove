befde833f8bea0013c3549ceee50e9bbb78a5078
孟君 <mengjun1990@gmail.com>
2015-07-07 08:34:56 +0800

    bind

--- gate.php
+++ gate.php
@@ -2,6 +2,6 @@
 if (isset($_REQUEST['backurl'])) {
 	$backurl = urlencode(base64_decode($_REQUEST['backurl']));
 } else {
-	$backurl = "http%3A%2F%2Fwap.datougou.cn%2Findex.php%2Fuser%2Findex%2Findex.html";
+	$backurl = "http%3A%2F%2Fwap.datougou.cn%2Findex.php";
 }
 header("Location: http://www.weixingate.com/gate.php?back=$backurl&force=1&info=force");
