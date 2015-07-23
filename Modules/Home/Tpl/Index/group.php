00156d6049cdcef0b18e252f9d310c8d0f4ed3ea
孟君 <mengjun1990@gmail.com>
2015-07-23 12:15:05 +0800

    bind

--- Modules/Home/Tpl/Index/group.php
+++ Modules/Home/Tpl/Index/group.php
@@ -3,15 +3,15 @@
 	<div class="item-wrap">
 		<div class="art_tit">北京上海所有微信群</div>
 	</div>
-	<img src="__PUBLIC__/images/bj1.png">
-	<img src="__PUBLIC__/images/bj2.png">
-	<img src="__PUBLIC__/images/bj3.png">
-	<img src="__PUBLIC__/images/bj4.png">
-	<img src="__PUBLIC__/images/bj5.png">
-	<img src="__PUBLIC__/images/bj6.png">
-	<img src="__PUBLIC__/images/sh1.png">
-	<img src="__PUBLIC__/images/sh2.png">
-	<img src="__PUBLIC__/images/sh3.png">
+	<img width="100%" src="__PUBLIC__/images/bj1.png">
+	<img width="100%" src="__PUBLIC__/images/bj2.png">
+	<img width="100%" src="__PUBLIC__/images/bj3.png">
+	<img width="100%" src="__PUBLIC__/images/bj4.png">
+	<img width="100%" src="__PUBLIC__/images/bj5.png">
+	<img width="100%" src="__PUBLIC__/images/bj6.png">
+	<img width="100%" src="__PUBLIC__/images/sh1.png">
+	<img width="100%" src="__PUBLIC__/images/sh2.png">
+	<img width="100%" src="__PUBLIC__/images/sh3.png">
 </div>
 
 <include file="./Tpl/Public/footer.php"/>
